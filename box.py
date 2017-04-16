8c1b3d761da6f1256eebf5454a60f2da90016a25
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-04-16 20:02:50 +0200

    Change Box constructor to admit a list and added a min_surface method

--- box.py
+++ box.py
@@ -1,8 +1,8 @@
 class Box:
-    def __init__(self, h, w, l):
-        self.__height = h
-        self.__width = w
-        self.__length = l
+    def __init__(self, dimensions):
+        self.__height = dimensions[0]
+        self.__width = dimensions[1]
+        self.__length = dimensions[2]
 
     def dimensions(self):
         return [self.__height, self.__width, self.__length]
@@ -18,9 +18,16 @@ class Box:
         self.__height = self.__width
         self.__width = self.__length
         self.__length = mem
+    
+    def min_surface(self):
+        h = self.__height 
+        w = self.__width 
+        l = self.__length
+        return min(h*w, h*l, l*w)
+        
 
 if __name__ == "__main__":
-    b = Box(10, 20, 30)
+    b = Box([10, 20, 30])
     print(b.dimensions())
     print(b.surface())
     print(b.get_height())
