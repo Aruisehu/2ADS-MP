5bacf0407df595605a81c6f607f5940035043d51
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-04-16 23:15:04 +0200

    Implement naive version of the problem

--- main.py
+++ main.py
@@ -15,8 +15,27 @@ def global_min_surface(boxes):
     #Retourne la surface minimum de toutes les boites données
     return min(list(map(lambda box: box.min_surface(), boxes)))
 
-#def naive_recursion(boxes):
+def naive_recursion(boxes, top_box, height, min_surface):
+    #top_surface représente la surface de la boite du haut de la pile
+    if (top_box.surface() == min_surface):
+        return height
+    surfaces = []
+    for box in boxes:
+        for i in range(3):
+            if box < top_box:
+                h = box.get_height()
+                surfaces.append(naive_recursion(boxes, box, height + h, min_surface))
+            box.rotate()
+    return max(surfaces) if len(surfaces) != 0 else height
 
 
 
-boxes = read_box_file("boxes.txt")
+#boxes = read_box_file("boxes.txt")
+boxes = []
+boxes.append(Box([10, 20, 30]))
+boxes.append(Box([5, 10, 50]))
+boxes.append(Box([100, 20, 1]))
+print(naive_recursion(boxes, Box([1000,1000,1000]), 0, global_min_surface(boxes)))
+
+
+
