19696a019650b2fece1845824016d057166f1e7b
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-05-08 00:30:34 +0200

    Fixed naive_recursion function and add greedy heuristics + some bugs fixs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from box import *
-
+"""
+    Projet fait par Yohann Jolain et Sébastien Erfani
+    Tous les algorithmes demandés parte du principe que la liste de boite est triée
+    La boite est trié via la fonction sort_boxes()
+"""
 def read_box_file(path):
     box_file = open(path, 'r')
     boxes = []
@@ -7,13 +11,14 @@ def read_box_file(path):
         #Lis la ligne et créer une liste avec les valeurs la composant
         dimensions = list(map(lambda x: int(x), line.split(" ")))
         boxes.append(Box(dimensions))
+    box_file.close()
     return boxes
 
 def get_key(box):
     return box.volume()
         
 def sort_boxes(boxes):
-    result = sorted(boxes, key = get_key, reverse=True)
+    result = sorted(boxes, key = get_key, reverse = True)
     return result
 
 def global_min_surface(boxes):
@@ -21,23 +26,36 @@ def global_min_surface(boxes):
     return min(list(map(lambda box: box.min_surface(), boxes)))
 
 def greedy_method(boxes):
+    # On part du principe simple que la boite avec le plus 
+    # grand volume est celle qui pourra avoir le plus de boite sur elle
+    # Tout en maximisant la hauteur
+    base = None # On a pas de boite qui compose la pile au début
+    tower = []
+    height = 0
+    for box in boxes:
+        for i in range(2):
+            box.set_max_height_state()
+            if (base == None) or (box < base):
+                base = box
+                tower.append(box)
+                height += box.get_height()
+            box.base_rotate()
+    return height, tower
 
+        
 
 def naive_recursion(boxes, top_box, height, min_surface):
     #top_box représente la boite en haut de la pile
-    if top_box != None: #Au départ il n'y a pas de boite
-        if (top_box.surface() == min_surface):
-            return height
-    surfaces = []
+    heights = []
     for box in boxes:
         for i in range(3):
             for j in range(2):
                 if top_box == None or box < top_box:
                     h = box.get_height()
-                    surfaces.append(naive_recursion(boxes, box, height + h, min_surface))
+                    heights.append(naive_recursion(boxes, box, height + h, min_surface))
                 box.base_rotate()
             box.rotate()
-    return max(surfaces) if len(surfaces) != 0 else height
+    return max(heights) if len(heights) != 0 else height
 
 
 #boxes = read_box_file("boxes.txt")
@@ -46,8 +64,8 @@ boxes.append(Box([10, 20, 30]))
 boxes.append(Box([5, 10, 50]))
 boxes.append(Box([100, 20, 1]))
 sorted_boxes = sort_boxes(boxes)
-print(sorted_boxes)
 print(naive_recursion(boxes, None, 0, global_min_surface(boxes)))
+print(greedy_method(boxes))
 
 
 
