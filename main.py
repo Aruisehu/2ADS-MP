12b7f229c5efb2fcba80e586c9b90c4bc7f6c1e1
Aruisehu <Yohann.JOLAIN@supinfo.com>
2017-05-08 11:39:04 +0200

    Now naive and greedy method fit with indication 1 in the subject + simplification of sort_boxes() method

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ def read_box_file(path):
     box_file.close()
     return boxes
 
-def get_key(box):
-    return box.volume()
-        
-def sort_boxes(boxes):
-    result = sorted(boxes, key = get_key, reverse = True)
-    return result
+def generate_all_boxes(boxes):
+    all_boxes = []
+    for box in boxes:
+        for i in range(3):
+            all_boxes.append(box)
+            box = box.rotate()
+    return all_boxes
 
-def global_min_surface(boxes):
-    #Retourne la surface minimum de toutes les boites données
-    return min(list(map(lambda box: box.min_surface(), boxes)))
+def sort_boxes(boxes):
+    result = sorted(boxes, key = lambda x: x.surface(), reverse = True)
+    return result
 
 def greedy_method(boxes):
     # On part du principe simple que la boite avec le plus 
@@ -41,21 +42,20 @@ def greedy_method(boxes):
                 height += box.get_height()
             box.base_rotate()
     return height, tower
-
         
-
-def naive_recursion(boxes, top_box, height, min_surface):
+def naive_recursion(boxes, top_box, height):
     #top_box représente la boite en haut de la pile
     heights = []
     for box in boxes:
-        for i in range(3):
-            for j in range(2):
-                if top_box == None or box < top_box:
-                    h = box.get_height()
-                    heights.append(naive_recursion(boxes, box, height + h, min_surface))
+        for j in range(2):
+            if top_box == None or box < top_box:
+                h = box.get_height()
+                heights.append(naive_recursion(boxes, box, height + h))
                 box.base_rotate()
-            box.rotate()
     return max(heights) if len(heights) != 0 else height
+
+def bottom_up(boxes):
+    base = None
 
 
 #boxes = read_box_file("boxes.txt")
@@ -63,9 +63,8 @@ boxes = []
 boxes.append(Box([10, 20, 30]))
 boxes.append(Box([5, 10, 50]))
 boxes.append(Box([100, 20, 1]))
-sorted_boxes = sort_boxes(boxes)
-print(naive_recursion(boxes, None, 0, global_min_surface(boxes)))
-print(greedy_method(boxes))
-
-
+all_boxes = generate_all_boxes(boxes)
+sorted_boxes = sort_boxes(all_boxes)
+print(naive_recursion(sorted_boxes, None, 0))
+print(greedy_method(sorted_boxes))
 
